43bf86005cbb904de64693dc4833b2aab3fd8824
Sam Johnson <sam@durosoft.com>
2018-10-14 02:55:57 -0400

    add exchange_token and get_transactions

--- src/plaid.cr
+++ src/plaid.cr
@@ -1,6 +1,73 @@
-# TODO: Write documentation for `Plaid`
-module Plaid
-  VERSION = "0.1.0"
+require "http/client"
+require "json"
 
-  # TODO: Put your code here
+module Plaid
+  @@mode = :sandbox
+  @@secret = ""
+  @@client_id = ""
+
+  PLAID_VERSION = "2018-05-22"
+  VALID_MODES = [:sandbox, :development, :production]
+
+  private def self.generate_headers
+    headers = HTTP::Headers.new
+    headers["Content-Type"] = "application/json"
+    headers["Plaid-Version"] = PLAID_VERSION
+    headers
+  end
+
+  private def self.endpoint(path)
+    "https://#{@@mode}.plaid.com#{path}"
+  end
+
+  def self.mode=(val : Symbol)
+    raise "mode must be one of #{VALID_MODES}" unless VALID_MODES.includes? val
+    @@mode = val
+  end
+
+  def self.secret=(secret : String)
+    @@secret = secret
+  end
+
+  def self.client_id=(client_id : String)
+    @@client_id = client_id
+  end
+
+  def self.exchange_token(public_token : String)
+    url = endpoint "/item/public_token/exchange"
+    form = JSON.build do |json|
+      json.object do
+        json.field "client_id", @@client_id
+        json.field "public_token", public_token
+        json.field "secret", @@secret
+      end
+    end
+    response = HTTP::Client.post url, generate_headers, form
+    JSON.parse response.body
+  end
+
+  def self.get_transactions(access_token : String, start_date : Time | String | Nil = nil, end_date : String | Nil = nil, count : Int32 | Nil = 500, offset : Int32 | Nil = 0)
+    url = endpoint "/transactions/get"
+    start_date = start_date.to_s("%F") if start_date.is_a? Time
+    end_date = end_date.to_s("%F") if end_date.is_a? Time
+    start_date ||= (Time.now - 500.years).to_s("%F")
+    end_date ||= (Time.now + 1.day).to_s("%F")
+    form = JSON.build do |json|
+      json.object do
+        json.field "client_id", @@client_id
+        json.field "secret", @@secret
+        json.field "access_token", access_token
+        json.field "start_date", start_date
+        json.field "end_date", end_date
+        json.field "options" do
+          json.object do
+            json.field "count", count
+            json.field "offset", offset
+          end
+        end
+      end
+    end
+    response = HTTP::Client.post url, generate_headers, form
+    JSON.parse response.body
+  end
 end
